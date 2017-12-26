732677c336788acc2f01763db65de5ac77f0a633
Reuel John <reueljohn@MAC077.local>
2017-12-26 15:24:07 +0800

    RecyclerView Done

--- app/src/main/java/com/example/reueljohn/first/GetUserInfo.java
+++ app/src/main/java/com/example/reueljohn/first/GetUserInfo.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -23,59 +25,53 @@ public class GetUserInfo extends AsyncTask<String, String, String> {
 
     //private TextView nameText, usernameText, addressText, companyText;
 
-
-    private RecyclerView recyclerView;
-    private UserListAdapter adapter;
+    private List<Users> users = new ArrayList<>();
     private Context context;
-
-    public GetUserInfo(Context context){
-        this.context = context;
-    }
 
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
-        List<Users> users = new ArrayList<>();
         try {
             JSONArray usersArray = new JSONArray(s);
-            for(int i = 0; i < usersArray.length(); i++){
+            for (int i = 0; i < usersArray.length(); i++){
                 JSONObject user = usersArray.getJSONObject(i);
                 JSONObject jAddress = user.getJSONObject("address");
                 JSONObject jCompany = user.getJSONObject("company");
                 Users newUser = new Users();
-            /*    newUser.name = user.getString("name");
+
+                newUser.name = user.getString("name");
                 newUser.username = user.getString("username");
                 newUser.street = jAddress.getString("street");
-                newUser.city =  jAddress.getString("city");
+                newUser.city = jAddress.getString("city");
                 newUser.company = jCompany.getString("name");
-                users.add(newUser);*/
 
-
-                try {
-                    newUser.name = user.getString("name");
-                    newUser.username = user.getString("username");
-                    newUser.street = jAddress.getString("street");
-                    newUser.city =  jAddress.getString("city");
-                    newUser.company = jCompany.getString("name");
-                    users.add(newUser);
-                }
-
-                catch (Exception e){
-                    e.printStackTrace();
-                }
+                users.add(newUser);
 
             }
-
-            recyclerView = (RecyclerView) recyclerView.findViewById(R.id.userList);
-            adapter = new UserListAdapter(context, users);
-            recyclerView.setAdapter(adapter);
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        /* try {
+            JSONArray usersArray = new JSONArray(s);
+            for(int i = 0; i < usersArray.length(); i++) {
+                JSONObject user = usersArray.getJSONObject(i);
+                *//*JSONObject jAddress = user.getJSONObject("address");
+                JSONObject jCompany = user.getJSONObject("company");*//*
+                Users newUser = new Users();
+                newUser.name = user.getString("name");
+                newUser.username = user.getString("username");
+               *//* newUser.street = jAddress.getString("street");
+                newUser.city = jAddress.getString("city");
+                newUser.company = jCompany.getString("name");*//*
+                users.add(newUser);
+                return;
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+            Log.d("b", "NOT WORKING");
+        }*/
     }
 
 
--- app/src/main/java/com/example/reueljohn/first/MainActivity.java
+++ app/src/main/java/com/example/reueljohn/first/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.reueljohn.first;
 
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -23,19 +26,57 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
 
+    private List<Users> users = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private UserListAdapter adapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new GetUserInfo(this).execute();
+        new FetchData().execute();
 
     }
 
+    private class FetchData extends AsyncTask<String,String ,String>{
 
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONArray usersArray = new JSONArray(s);
+                for (int i = 0; i < usersArray.length(); i++) {
+                    JSONObject user = usersArray.getJSONObject(i);
+                    JSONObject jAddress = user.getJSONObject("address");
+                    JSONObject jCompany = user.getJSONObject("company");
+                    Users newUser = new Users();
 
+                    newUser.name = user.getString("name");
+                    newUser.username = user.getString("username");
+                    newUser.street = jAddress.getString("street");
+                    newUser.city = jAddress.getString("city");
+                    newUser.company = jCompany.getString("name");
 
+                    users.add(newUser);
 
+                }
 
+                recyclerView = (RecyclerView) findViewById(R.id.userList);
+                adapter = new UserListAdapter(MainActivity.this, users);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
+                
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return NetworkConnect.getUsers();
+        }
+    }
 }
